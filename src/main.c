930d5d2327bc91ccdfe3d544657c468d4b0cc89b
Fauth-Mayer Paul Arthur <pf030@hdm-stuttgart.de>
2020-12-08 00:28:05 +0100

    apply clang format

--- src/main.c
+++ src/main.c
@@ -1,26 +1,24 @@
+#include "helpers.h"
 #include <avr/io.h>
 #include <util/delay.h>
-#include "helpers.h"
 
-int main(void)
-{
-    // runce once
-    // set which pins to use as output in the Data Direction Register
-    DDRB = (1 << PB5);
+int main(void) {
+  // runce once
+  // set which pins to use as output in the Data Direction Register
+  DDRB = (1 << PB5);
 
-    //switch on LEDs
-    while (1)
-    {
-        setBit(PORTB, PB5);
-        _delay_ms(200);
-        clearBit(PORTB, PB5);
-        _delay_ms(200);
-        toggleBit(PORTB, PB5);
-        _delay_ms(200);
-        toggleBit(PORTB, PB5);
-        _delay_ms(1000);
-    }
-    // can never be reached
-    return 0;
+  // switch on LEDs
+  while (1) {
+    setBit(PORTB, PB5);
+    _delay_ms(200);
+    clearBit(PORTB, PB5);
+    _delay_ms(200);
+    toggleBit(PORTB, PB5);
+    _delay_ms(200);
+    toggleBit(PORTB, PB5);
+    _delay_ms(1000);
+  }
 
+  // can never be reached
+  return 0;
 }
