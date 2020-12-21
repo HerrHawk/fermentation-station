e5ec4020fb3a75fea81abd7cb0f3dddbe3259ec4
Fauth-Mayer Paul Arthur <pf030@hdm-stuttgart.de>
2020-12-21 22:13:54 +0100

    define display dimensions as globals

--- src/globals.h
+++ src/globals.h
@@ -1,4 +1,6 @@
 #define __AVR_ATmega328P__
 #define F_CPU 12000000UL
-#define MAXTEMP = 70
-#define ERRORTEMP = 80
+#define MAXTEMP 70
+#define ERRORTEMP 80
+#define DISPLAY_WIDTH 126
+#define DISPLAY_HEIGHT 298
