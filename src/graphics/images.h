7dbb8c73cdef485a4d1a53660c120357e8d5d42f
Fauth-Mayer Paul Arthur <pf030@hdm-stuttgart.de>
2021-01-30 16:41:56 +0100

    introduce icons

--- src/graphics/images.h
+++ src/graphics/images.h
@@ -5,3 +5,8 @@ extern const unsigned char smiley[] PROGMEM;
 extern const unsigned char checkers[] PROGMEM;
 extern const unsigned char dickbutt[] PROGMEM;
 extern const unsigned char doge[] PROGMEM;
+extern const unsigned char temp_icon[] PROGMEM;
+extern const unsigned char hum_icon[] PROGMEM;
+extern const unsigned char start_icon[] PROGMEM;
+extern const unsigned char pow_icon[] PROGMEM;
+extern const unsigned char back_icon[] PROGMEM;
