99fb5829ceb6342f5b687bbc3ce1dd66926479aa
Fauth-Mayer Paul Arthur <pf030@hdm-stuttgart.de>
2021-01-31 19:05:33 +0100

    disable new logging function

--- src/logging.h
+++ src/logging.h
@@ -26,7 +26,7 @@ typedef enum
 const int const sector_logging_enabled[];
 const char* const sector_names[];
 
-#define log(...) fprintf(&uart_output, __VA_ARGS__)
+// #define log(...) fprintf(&uart_output, __VA_ARGS__)
 
 #ifdef DEBUG
 #define LOG_DEBUG(sector, ...)                                                                     \
